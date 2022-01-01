1fa10ac77d15f9af70744aa3ef7558542eee06bd
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-01 21:48:55 +0630

    feat: add CommandContext type

--- src/services/discord/index.ts
+++ src/services/discord/index.ts
@@ -1,9 +1,20 @@
-import { Client, Intents } from "discord.js"
+import {Client, Guild, Intents, Message} from "discord.js"
 import {parseCommand} from "./parse";
 import * as path from "path";
 import * as fs from "fs";
+import {PrismaClient} from '@prisma/client'
 
 const handlerCache = new Map<string, any>()
+const prisma = new PrismaClient()
+
+export type CommandContext = {
+    prisma: PrismaClient,
+    discord: Client,
+    message: Message,
+    guild: Guild | null,
+    args: any[]|Record<any, any>
+}
+
 export const discord = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -29,19 +40,31 @@ discord.on("messageCreate", async (message) => {
     }
 
     let handlerPath = path.join(process.cwd(), 'src', command?.type, command?.command + '.ts')
-    if (!fs.existsSync(handlerPath)) {
+    let handler = handlerCache[handlerPath]
+
+    if (!handler && !fs.existsSync(handlerPath)) {
         await error()
         return
     }
 
-    let handler = handlerCache[handlerPath]
+
     if (!handler) {
         handler = await import(handlerPath)
         handlerCache[handlerPath] = handler
+        if (handler?.alias)
+            handlerCache[handlerPath.replace(command?.command + '.ts', handler?.alias + '.ts')] = handler
     }
 
     try {
-        await handler?.handle(discord, message, command?.args)
+        let context: CommandContext = {
+            guild: message.guild,
+            discord,
+            message,
+            prisma,
+            args: command?.args
+        }
+
+        await handler?.handle(context)
     } catch (e) {
         await message.reply('Command failed!')
     }
