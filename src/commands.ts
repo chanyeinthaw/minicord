f6d4c3fff47af0fa8f3d2b88409d60f869baf588
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-02 17:28:29 +0630

    feat: add command aliases

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,7 @@
 import {app} from "@app/index";
 
 app.on('create-space', require('@handlers/create-space').default)
+    .alias('cs')
 
 app.on('add-exclusive-roles', require('@handlers/add-exclusive-roles').default)
+    .alias('aer')
