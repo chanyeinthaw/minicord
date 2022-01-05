1ca9ef039cc43d522efc361ebef78a3fa498147d
Chan Nyein Thaw <me@chanyeinthaw.com>
2022-01-05 21:28:00 +0630

    feat: root unroot commands

--- src/commands/middlewares/auth.ts
+++ src/commands/middlewares/auth.ts
@@ -1,8 +1,15 @@
 import {CommandContext} from "@lib/mini-command";
 import {Permissions} from "discord.js";
 
-export default function auth(ctx: CommandContext) {
+export default async function auth(ctx: CommandContext) {
     let isAdmin = ctx.message.member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR) ?? false
+    let userId = ctx.message.member?.id
+
+    let user = await ctx.prisma.user.findFirst({
+        where: { id: userId }
+    })
+
+    if (user && user.isRoot) return
 
     if (!isAdmin) throw new Error('You can\'t use this command.')
 }
--- src/registry.ts
+++ src/registry.ts
@@ -5,5 +5,55 @@ import '@handlers/spaces'
 
 import {app} from "@app/index";
 import setup from "@handlers/setup";
+import {checkUsers} from "@handlers/spaces/shared/check-users";
 
 app.on('setup', setup)
+app.on('root :userId', async (ctx) => {
+    let {userId} = ctx.params
+
+    checkUsers([userId], ctx.guild)
+
+    let user = await ctx.prisma.user.findFirst({ where: { id: userId } })
+    if (!user) {
+        await ctx.prisma.user.create({
+            data: {
+                id: userId,
+                isRoot: true
+            }
+        })
+    } else {
+       await ctx.prisma.user.update({
+           where: { id: userId },
+           data: {
+               isRoot: true
+           }
+       })
+    }
+
+    return ctx.message.reply('\`Rooted!\`')
+})
+
+app.on('unroot :userId', async (ctx) => {
+    let {userId} = ctx.params
+
+    checkUsers([userId], ctx.guild)
+
+    let user = await ctx.prisma.user.findFirst({ where: { id: userId } })
+    if (!user) {
+        await ctx.prisma.user.create({
+            data: {
+                id: userId,
+                isRoot: false
+            }
+        })
+    } else {
+        await ctx.prisma.user.update({
+            where: { id: userId },
+            data: {
+                isRoot: false
+            }
+        })
+    }
+
+    return ctx.message.reply('\`Unrooted!\`')
+})
